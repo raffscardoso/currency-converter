63b0d99eff7de6b153acd7de1edb9ffcf912e69c
Rafael Cardoso <raffs.cardoso15@gmail.com>
2024-07-04 19:05:58 -0300

    starting form construction

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { CurrencyFormComponent } from './components/currency-form/currency-form.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, CurrencyFormComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'currency-converter';
---
+++ src/app/components/currency-form/currency-form.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-currency-form',
+  standalone: true,
+  imports: [],
+  templateUrl: './currency-form.component.html',
+  styleUrl: './currency-form.component.css'
+})
+export class CurrencyFormComponent {
+
+}
